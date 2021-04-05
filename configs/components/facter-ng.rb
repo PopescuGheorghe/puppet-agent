84cd62da9c7703e4c71c455eb2ea5a0031afcb91
gimmy <gheorghe.popescu@puppet.com>
2021-04-05 16:45:58 +0300

    (PA-3570) convert facter-ng to a git component
    In order to be able to use the latest commit from
    facter#main we need to have facter-ng as a git
    component.

--- configs/components/facter-ng.rb
+++ configs/components/facter-ng.rb
@@ -1,7 +1,6 @@
 component "facter-ng" do |pkg, settings, platform|
-  pkg.version '4.0.50'
-  pkg.sha256sum '2c5fed877f4cc2480243b3f249304356e335551f0399c0994f374939a0ec8aa2'
-  pkg.url "https://github.com/puppetlabs/facter/archive/#{pkg.get_version}.tar.gz"
+  pkg.load_from_json('configs/components/facter-ng.json')
+  pkg.dirname("facter-ng")
 
   pkg.build_requires "puppet-runtime"
   pkg.build_requires "pl-ruby-patch"
@@ -17,15 +16,6 @@ component "facter-ng" do |pkg, settings, platform|
   # will fail by blowing out the stack.
   if settings[:ruby_vendordir]
     pkg.environment "RUBYLIB", "#{settings[:ruby_vendordir]}:$(RUBYLIB)"
-  end
-
-
-  pkg.configure do
-    [
-        "cd ..",
-        "mv facter-#{pkg.get_version}/* #{pkg.get_version}",
-        "cd #{pkg.get_version}"
-    ]
   end
 
 
